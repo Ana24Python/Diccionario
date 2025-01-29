2135a7aebd0c4bb91b8f0e81ca8099c3681aaf32
Ana24Python <ana.suelta@outlook.com>
2025-01-28 20:08:20 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from bot_logic import gen_pass
+from bot_logic import gen_emodji
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
@@ -19,8 +19,8 @@ async def on_message(message):
         await message.channel.send("Hi!")
     elif message.content.startswith('$bye'):
         await message.channel.send("\U0001f642")
-    elif message.content.startswith('$contraseña'):
-        await message.channel.send((gen_pass(10)))
+    elif message.content.startswith('$emoji'):
+        await message.channel.send(gen_emodji())
     else:
         await message.channel.send(message.content)
 
